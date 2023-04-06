93069b2d4263b848c4cdd1bbc746c034d8ebf029
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-04-05 21:31:53 -0300

    Atualiza

--- 1_INTRO/2_utilizando_modulo/index.js
+++ 1_INTRO/2_utilizando_modulo/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs') //file system
 
-fs.readFile('arquivo','utf8',(err,data) =>{ //Funçao para leitura de arquivos
+fs.readFile('arquivo.txt','utf8',(err,data) =>{ //Funçao para leitura de arquivos
     if(err){
         console.log(err)
     }
