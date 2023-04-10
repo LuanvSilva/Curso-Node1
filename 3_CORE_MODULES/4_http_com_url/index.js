8a5065b1c8a49dfc3ed0e97c6d5004f7e3537059
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-04-10 19:23:20 -0300

    Atualiza

--- 3_CORE_MODULES/4_http_com_url/index.js
+++ 3_CORE_MODULES/4_http_com_url/index.js
@@ -2,18 +2,19 @@ const http = require('http')
 
 const port = 3000
 
-const server = http.createServer((req,res) =>{
+const server = http.createServer((req,res) =>{ //criar servidor basico
 const urlInfo = require('url').parse(req.url,true)
 const name = urlInfo.query.name
 
-
-
 res.statusCode = 200
-res.headersSent('Contenty-Type, text/html')
+res.setHeader('Contenty-Type' ,'text/html')
 if(!name){
- res.end('<h1>Preecha o seu nome:</h1><form method="GET"<input type="text" name="name"')
+ res.end('<h1>Preecha o seu nome:</h1><form method="GET"><input type="text" name="name" />')
 }else{
-
+ res.end(`<h1>Seja bem vindo  ${name}! </h1>`) 
 }
 
 }) 
+server.listen(port,() =>{
+    console.log(`O servidor esta rodando na porta ${port}` )
+})
