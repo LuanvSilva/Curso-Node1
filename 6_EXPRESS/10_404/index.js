5e17da8a21137967164528425e214c0f7ec70973
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-04-13 21:34:40 -0300

    atualizacao

---
+++ 6_EXPRESS/10_404/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path =  require('path')
+const users = require('./users')
+
+
+app.use(
+    express.urlencoded({
+        extended:true,
+    })
+)
+app.use(express.json())
+//arquivos estaticos
+
+app.use(express.static('public'))
+
+const basePath = path.join(__dirname, 'tamplates')
+
+app.use('/users',users)
+
+app.get('/',(req,res) =>{
+  
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.use((req,res,next) =>{
+res.status(404).sendFile(`${basePath}/404.html`)
+
+}) 
+     
+app.listen(port, ()=>{
+    console.log(`App rodando na porta ${port}`)
+})
+
