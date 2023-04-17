8cad1358ed6b1def51e1696ebfe93e3bc90d0e26
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-04-17 20:30:37 -0300

    atualiza

--- 7_acconutss/index.js
+++ 7_acconutss/index.js
@@ -18,14 +18,14 @@ function operation(){
         'Sair',
     ],
 
-  },]).then((ansewer) =>{
-    const action = ansewer['action']
+  },]).then((answer) =>{
+    const action = answer['action']
     console.log(action)
 
     if(action === 'Criar Conta'){
         createAccount()
     }else if(action === 'Depositar'){
-        deposit()
+       deposit()
     }else if(action === 'Consutar Saldo'){
 
     }else if(action === 'Depositar'){
@@ -38,19 +38,20 @@ function operation(){
   }).catch((err) =>{console.log(err)})
 }
 
+//FUNÇAO RESPONSAVEL PELO INICIO 
 function createAccount(){
     console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
     console.log(chalk.green('define as opções da sua conta a seguir '))
     buildAccount()
 }
-
+//FUNCAO RESPONSAVEL POR CRIAR UMA CONTA //VERIFICA SE JA EXISTE SE NAO CRIA UMA NOVA
 function buildAccount(){
     inquirer.prompt([{
         name:'accountName',
         message:'Digite um nome para sua conta ',
 
-    },]).then((ansewer)=>{
-        const accountName = ansewer['accountName']
+    },]).then((answer)=>{
+        const accountName = answer['accountName']
         console.info(accountName)
 
         if(!fs.existsSync('accounts')){
@@ -70,6 +71,71 @@ function buildAccount(){
 
     }).catch((err) => {console.log(err)})
 }
+
+//FUNÇAO DE DEPOSITO
+
 function deposit(){
+    inquirer.prompt([{
+        name:'accountName',
+        message:'Qual o nome da sua conta',
+
+
+    },]).then((answer) =>{
+        const accountName = answer['accountName']
+        if(!checkAccount(accountName)){
+            return deposit()
+        }
+        inquirer.prompt([{
+            name:'amount',
+            message:'Quantos voce deseja depositar',
+        },]).then((answer) =>{
+            const amount = answer['amount']
+            addAmount(accountName,amount)
+            operation()
+
+        }).catch((err) =>{console.log(err)})
+ 
+    }).catch((err) =>{console.log(err)})
+}
+
+//FUNÇAO RESPONSAVEL POR VERIFICAR SE CONTA JA EXISTE
+function checkAccount(accountName){
+
+    if(!fs.existsSync(`accounts/${accountName}.json`)){
+        console.log(chalk.bgRed.black( 'Esta Conta nao existe, escolha outro nome'))
+    return false
+    } 
+
+    return true 
+}
+
+//RESPONSAVEL POR VERIFICAR SE TEM ERRO NA DEPOSITO E FAZER O DEPOSITO NA CONTA
+function addAmount(accountName,amount){
+
+    const accountDate = getAccount(accountName)
+    if(!amount){
+        console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente'))
+        return operation()
+    }
+
+    accountDate.balance = parseFloat(amount) + parseFloat(accountDate.balance)
+
+    fs.writeFileSync(`accounts/${accountName}.json`,
+    JSON.stringify(accountDate), function (err) {
+        console.log(err)
+    }
+    )
+ console.log(chalk.green(`Foi depositado o valor de ${amount} na sua conta!`))
+ 
+}
+//FUNÇAO RESPONSAVEL POR FAZER A LEITURA DA CONTA
+function getAccount(accountName){
+const accountJSON = fs.readFileSync(`accounts/${accountName}.json`,{
+    encoding:'utf-8',
+    flag: 'r',
     
+})
+
+return JSON.parse(accountJSON)
+
 }
