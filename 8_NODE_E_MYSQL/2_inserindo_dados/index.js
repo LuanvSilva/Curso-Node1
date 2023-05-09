b73a504d16940a3fa514df6928f89a9e56300cf8
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-05-09 20:54:08 -0300

    atualizacao

--- 8_NODE_E_MYSQL/2_inserindo_dados/index.js
+++ 8_NODE_E_MYSQL/2_inserindo_dados/index.js
@@ -3,13 +3,33 @@ const exphbs = require('express-handlebars')
 const mysql = require('mysql2')
 
 const app = express()
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
 
-
-
-
+app.use(express.json())
+app.engine('handlebars',exphbs.engine())
+app.set('view engine','handlebars')
 
 app.get('/', (req,res) =>{
     res.render('home')
+})
+
+app.post('/books/insertbook',(req,res) =>{
+const title = req.body.title
+const pageqty = req.body.pageqty
+const sql = `INSERT INTO books (title,pageqty) VALUES ('${title}','${pageqty}')`
+
+conn.query(sql,(err) =>{
+    if(err) {
+        console.log(err)
+    }
+        res.redirect('/')
+    
+})
+
 })
 const conn = mysql.createConnection({
     host: 'localhost',
@@ -21,7 +41,7 @@ const conn = mysql.createConnection({
 conn.connect(function(err){
    if(err){ console.log(err)
    }else{
-    console.log('connected')
+    console.log('connectou ao Mysql')
     app.listen(3000)
    }
 })
