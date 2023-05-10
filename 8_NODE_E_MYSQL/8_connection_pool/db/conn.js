11c5d59555168fe027e5df845c737c737953496b
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-05-10 19:26:02 -0300

    atualiza

---
+++ 8_NODE_E_MYSQL/8_connection_pool/db/conn.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2')
+
+
+const pool = mysql.createPool({
+    connectionLimit: 10 ,
+    host: 'localhost',
+    user: 'root',
+    password: 'Luan2023',
+    database: 'nodemysql'
+})
+
+module.exports =  pool
