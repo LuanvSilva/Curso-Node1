4a218ba0591427d63c02293eef360213b25addc2
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-05-11 20:59:23 -0300

    atualizacao

--- 6_EXPRESS/3_render_html/index.js
+++ 6_EXPRESS/3_render_html/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 const path =  require('path')
 
-const basePath = path.join(__dirname, 'tamplates')
+const basePath = path.join(__dirname, 'tamplatess')
 app.get('/',(req,res) =>{
     
     res.sendFile(`${basePath}/index.html`)
--- 8_NODE_E_MYSQL/0_projeto/index.js
+++ 8_NODE_E_MYSQL/0_projeto/index.js
@@ -1,12 +1,45 @@
 const express = require('express');
-const app = express();
+const mysql = require('mysql2')
+const app = express()
+const path = require('path')
 
+const basePath = path.join(__dirname, 'tamplates')
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.htm');
+    const name = req.body.nome 
+    const email = req.body.email
+    const senha = req.body.senha
+    const telefone = req.body.telefone
+    const data = req.body.data
+
+    const sql = `INSERT INTO cadastro (Nome,Email,Senha,Telefone,data) VALUES ( '${nome}','${email}','${senha}','${telefone}','${data}')`
+    conn.query(sql,(err)=>{
+        if(err){
+            console.log(err)
+
+            }else{
+                res.redirect('/')
+                
+        }
+    })
 
 });
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Luan2023',
+    database: 'nodemysql'
 
-app.listen(3000, () => {
-    console.log('El servidor está en funcionamiento en el puerto 3000');
+    })
+conn.connect((err) => { 
+    if (err){
+        console.log(err)
+    }
+    console.log('Conectado ao banco')
+    app.listen(3000, () => {
+    
 });
+    })
+
+
+
