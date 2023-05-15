d87b37086dba8ea955dbaae6516c0525f26e4939
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-05-15 20:04:21 -0300

    formulario conctado ao banco

--- 8_NODE_E_MYSQL/0_projeto/index.js
+++ 8_NODE_E_MYSQL/0_projeto/index.js
@@ -29,7 +29,49 @@ app.post('/books', (req, res) => {
         }
     });
 });
+app.get('/update', (req, res) => {
+    res.sendFile(`${basePath}/update.html`);
+});
 
+app.post('/update',(req,res) =>{
+ const id = req.body.id;
+ const nome = req.body.nome;
+ const email = req.body.email;
+ const senha = req.body.senha;
+ const telefone = req.body.telefone;
+ const data = req.body.data;
+
+ const sql = `UPDATE cadastro SET Nome = '${nome}', Email = '${email}', Senha = '${senha}', Telefone = '${telefone}', Nascimento = '${data}' WHERE id = ${id}`
+ conn.query(sql, (err) => {
+    if (err) {
+        console.log(err);
+        res.sendStatus(500);
+        } else {
+            res.redirect('/');
+            }
+});
+app.get('/delete', (req, res) => {
+    res.sendFile(`${basePath}/delete.html`);
+});
+
+app.post('/delete',(req,res) =>{
+    const id = req.body.id;
+    const sql = `DELETE FROM cadastro WHERE id = ${id}`
+    conn.query(sql, (err) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            } else {
+                res.redirect('/');
+                }
+});
+})
+
+
+
+
+
+})
 const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
