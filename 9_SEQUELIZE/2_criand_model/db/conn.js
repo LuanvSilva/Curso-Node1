48ae6f94e722cea402198da9dbddf5ede308b478
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-05-18 17:31:35 -0300

    conctando sequelize com o mysql

---
+++ 9_SEQUELIZE/2_criand_model/db/conn.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodesequelize','root','Luan2023',{
+    host: 'localhost',
+    dialect: 'mysql',
+})
+
+try {
+    
+    sequelize.authenticate()
+    console.log('Conectamos com sucesso com sequelize!')
+} catch (error) {
+    console.log('Não foi possivel conectar: ', error)
+}
+
+module.exports = sequelize
