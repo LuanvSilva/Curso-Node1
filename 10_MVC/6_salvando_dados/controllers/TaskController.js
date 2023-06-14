7c011a27b0fc1f7ce522cba0781f3aafff1a4caa
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-06-14 19:50:00 -0300

    atualização

--- 10_MVC/6_salvando_dados/controllers/TaskController.js
+++ 10_MVC/6_salvando_dados/controllers/TaskController.js
@@ -3,6 +3,17 @@ const Task = require('../models/task')
 module.exports = class TaskController {
     static createTasks(req, res) {
         res.render('tasks/create')
+
+    }
+    static async createTaskSave(req, res){
+
+        const task = {
+            title: req.body.title,
+            description: req.body.description,
+            done: false
+        }
+        await Task.create(task)
+        res.redirect('/tasks')
     }
 
     static showTasks(req, res) {
--- 10_MVC/6_salvando_dados/routes/tasksRoutes.js
+++ 10_MVC/6_salvando_dados/routes/tasksRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const TaskController = require('../controllers/TaskController')
 
 router.get('/add', TaskController.createTasks)
+router.post('/add', TaskController.createTaskSave)
 router.get('/', TaskController.showTasks)
 
 module.exports = router
