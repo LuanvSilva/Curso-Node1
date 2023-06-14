dda019907e9f9eb445bfe9b40f233b44cfd6650f
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-06-14 20:00:43 -0300

    10

--- 10_MVC/6_salvando_dados/models/task.js
+++ 10_MVC/6_salvando_dados/models/task.js
@@ -5,15 +5,15 @@ const db = require('../db/conn')
 const Task = db.define('Task', {
     title:{
         type: DataTypes.STRING,
-        required: true
+        allowNull : false,
     },
     description:{
         type: DataTypes.STRING,
-        required: true
+        
     },
     done:{
         type: DataTypes.BOOLEAN,
-        required: true
+      
     },
 
 })
