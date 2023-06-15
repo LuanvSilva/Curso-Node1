0d2f47fb7f93447522baba997cdd06a8f481d80e
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-06-15 20:09:47 -0300

    atualiza

--- 11_TOUGHTS/index.js
+++ 11_TOUGHTS/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const exphs = require('express-handlebars')
+const exphbs = require('express-handlebars')
 const session = require('express-session')
 const FileStore = require('session-file-store')(session)
 const flash = require('express-flash')
@@ -8,13 +8,57 @@ const app = express()
 
 const conn = require('./db/conn')
 
-app.engine('handlebars', exphbs.engine())
-app.set('view engine', 'handlebars')
+//recebe resposta do body
 app.use(
     express.urlencoded({
         extended: true
-    })  
+    })
 )
+app.use(express.json())
+
+//template engine
+app.engine('handlebars',exphbs.engine())
+app.set('view engine','handlebars')
+
+// session middleware
+app.use(
+    session({
+        name: "session",
+        secret: "nosso_secret",
+        resave: false,
+        saveUninitialized:false,
+        store : new FileStore({
+            logFn: function() {},
+            path: require('path').join(require('os').tmpdir(), 'session')
+        }),
+        cookie:{
+            secure:false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true
+        }
+    }),
+)
+
+//flash mensagem
+app.use(flash())
+
+//set session to res
+app.use((req,res,next)=>{
+
+    if(req.session.userid){
+        res.locals.session = req.session
+    }
+    next()
+
+})
+
+
+
+
+
+
+
 
 conn
 .sync()
