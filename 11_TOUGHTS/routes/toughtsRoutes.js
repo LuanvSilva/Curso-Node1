89da316e5a0a138a2f8409cbfc786b690aa4d71c
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-06-17 14:52:16 -0300

    Atualizacção de toughts

--- 11_TOUGHTS/routes/toughtsRoutes.js
+++ 11_TOUGHTS/routes/toughtsRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const ToughtsController = require('../controllers/ToughtsController')
+const checkAuth = require('../helpers/auth').checkAuth
 
+router.get('/add',checkAuth, ToughtsController.createTought)
+router.post('/add',checkAuth, ToughtsController.createToughtSave)
+router.get('/dashbord',checkAuth, ToughtsController.dashboard)
 router.get('/', ToughtsController.showToughts)
 
 module.exports = router
