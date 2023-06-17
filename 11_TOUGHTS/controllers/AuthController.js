ba9d0cc73c4238a4fc1bac296dbc085315cfe0af
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-06-17 12:51:12 -0300

    atualização de login do usuario

--- 11_TOUGHTS/controllers/AuthController.js
+++ 11_TOUGHTS/controllers/AuthController.js
@@ -5,13 +5,29 @@ module.exports = class AuthController {
     static login(req,res){
         res.render('auth/login')
     }
+    static async loginPost(req,res){
+        const {email, password} = req.body
+        //find user
+        const user = await User.findOne({where:{email: email}})
+        if(!user){
+            req.flash('message','Usuario Não encontrado!')
+            res.render('auth/register')
+            return
+        }
+        //check if password math
+        const passwordMatch = await bcrypt.compareSync(password, user.password)
+        if(!password){
+            req.flash('message','Senha incorreta!')
+            res.render('auth/login')
+        }
+    }
     static register(req,res){
         res.render('auth/register')
     }
     static async registerPost(req,res){
 
-        const {name, email, password, confirmPassword} = req.body
-        if(!password ||!confirmPassword) {
+        const {name, email, password, confirmpassword} = req.body
+        if(password != confirmpassword) {
             req.flash('message','As senhas não conferem, tente novamente!')
             res.render('auth/register')
             return
@@ -32,11 +48,24 @@ module.exports = class AuthController {
             password: hashedPassword
         }
         try {
-            await User.create(user)
+          const createdUser = await User.create(user)
+
+            //inicializar a sessão
+            req.session.userid = createdUser.id;
+
             req.flash('message','Cadastro realizado com sucesso!')
-            res.redirect('/')
+
+            req.session.save(() =>{
+                res.redirect('/')
+            })
+            
+
         } catch (error) {
             console.log(error)
         }
+    }
+    static logout(req,res){
+        req.session.destroy()
+        res.redirect('/login')
     }
 }
