5c33caf4e3508fde65602b117282e64dc4adf161
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-06-27 18:28:57 -0300

    main4.0

--- 15_GET_A_PET/backend/controllers/UserController.js
+++ 15_GET_A_PET/backend/controllers/UserController.js
@@ -1,6 +1,10 @@
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+//Helpers
 const createUserToken = require('../helpers/create-user-token')
+const getToken = require('../helpers/get-token')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -116,10 +120,27 @@ module.exports = class UserController {
         let currentUser
         console.log(req.headers.authorization)
         if(req.headers.authorization){
+            const token = getToken(req)
+            const decoded = jwt.verify(token, 'nossosecret')
+
+            currentUser = await User.findById(decoded.id)
+            currentUser.password = undefined
 
         }else{
             currentUser=null;
         }
         res.status(200).send(currentUser)
+    }
+
+    static async getUserById(req,res){
+        const id = req.params.id
+
+        const user = await User.findById(id).select('-password')
+
+        if(!user){
+            res.status(422).json({message: 'Usuario não encontrado'})
+            return
+        }
+        res.status(200).json({ user })
     }
 }
